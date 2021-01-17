3ccf6d181e67f2c8c09c7c74869453317722e4d6
Fauzan My <bangpozan@gmail.com>
2021-01-17 15:14:19 +0700

    Update toggle-vanilla-javascript.js

--- toggle-vanilla-javascript.js
+++ toggle-vanilla-javascript.js
@@ -1,16 +1,16 @@
-	<script>
+<script>
 
-		let burger = document.querySelector('.burger');
-		let close = document.querySelector('.close');
-		let sidenav = document.querySelector('#sidenav');
+	let burger = document.querySelector('.burger');
+	let close = document.querySelector('.close');
+	let sidenav = document.querySelector('#sidenav');
 
-		// Burger click function
-		burger.addEventListener('click', function () {
-			sidenav.classList.add('active');
-		});
-		// Close button click function
-		close.addEventListener('click', function () {
-			sidenav.classList.remove('active');
-		});
+	// Burger click function
+	burger.addEventListener('click', function () {
+		sidenav.classList.add('active');
+	});
+	// Close button click function
+	close.addEventListener('click', function () {
+		sidenav.classList.remove('active');
+	});
 
-	</script>
+</script>
